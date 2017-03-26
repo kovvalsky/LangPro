95f4d996f5e6e44bd59488e1e0f399437310208d
kovvalsky <lasha.abzianidze@gmail.com>
2017-03-26 03:01:08 +0200

    knowledge induction start

---
+++ kb_induction.pl
@@ -0,0 +1,4 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Induction of lexical relations from an open tableau
+% which cause the tableu to close according to the gold answer
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
