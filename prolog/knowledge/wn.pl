98ff58adb62f099bd81e771f8bd937b63183121b
kowalsky <lasha.abzianidze@gmail.com>
2021-06-28 15:50:08 +0200

    make WN inclusion more modular

--- prolog/knowledge/wn.pl
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-:- ensure_loaded([
-	'../../WNProlog/wn_hyp',
-	%'wn_h_',
- 	'../../WNProlog/wn_sim',
-	'../../WNProlog/wn_ant',
-	'../../WNProlog/wn_der',
-	'../../WNProlog/wn_s'
-	]).
