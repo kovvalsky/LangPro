d2a03e3c828ed1967a95d9e9ed373ebe9b500fcc
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2024-07-21 16:20:20 +0200

    add induced knowledge for c&c

---
+++ results/starSEM_2020/TE/ccg/TD_E/al,ch,w3,-k,r800,c20_ab,ch,cKB,cT,p123.ans_KB.pl
@@ -0,0 +1,318 @@
+%%%%%%%%%%%%% Parameters of parList %%%%%%%%%%%%%%
+%%% [nil,effCr([equi,nonBr,nonProd,nonCons]),allInt,waif(results/starSEM_2020/TE/ccg/TD_E/al,ch,w3,-k,r800,c20_ab,ch,cKB,cT,p123.ans),complete_tree,aall,constchk,wn_ant,wn_sim,wn_der,no_kb,ral(800),parallel(20)]
+
+%%%%%%%%%%%%%%%% Induction config %%%%%%%%%%%%%%%%
+%%% [align-both,constchk,constKB,compTerms,patterns-[_148,_154@_156,_166@_168@_174,_190@(_184@_186)]]
+%%%%%%%%%%%%%%% Induced Knowledge %%%%%%%%%%%%%%%%
+ind_rel(isa_wn(young,'young look')).
+ind_rel(isa_wn(yard,backyard)).
+ind_rel(isa_wn(yard,'back yard')).
+ind_rel(isa_wn(wood,'wooded area')).
+ind_rel(isa_wn(wood,wooded)).
+ind_rel(isa_wn(woman,lady)).
+ind_rel(isa_wn(woman,girl)).
+ind_rel(isa_wn(woman,'elegant woman')).
+ind_rel(isa_wn(woman,couple)).
+ind_rel(isa_wn(woman,cook)).
+ind_rel(isa_wn('with a@knife','use a@knife')).
+ind_rel(isa_wn(with,in)).
+ind_rel(isa_wn(wipe,spread)).
+ind_rel(isa_wn('wedding dress',veil)).
+ind_rel(isa_wn('wear@(a@beret)','ride the@bicycle')).
+ind_rel(isa_wn(wear,dance)).
+ind_rel(isa_wn(watch,attend)).
+ind_rel(isa_wn(wall,'brick wall')).
+ind_rel(isa_wn('walk up','walk down')).
+ind_rel(isa_wn(walk,'move around')).
+ind_rel(isa_wn(wakeboard,waterskie)).
+ind_rel(isa_wn(vehicle,car)).
+ind_rel(isa_wn(turn_around,go_around)).
+ind_rel(isa_wn('trek slowly','in@(s@wood) hike')).
+ind_rel(isa_wn(toy,stuffed)).
+ind_rel(isa_wn(toddler,'young kid')).
+ind_rel(isa_wn('to a@(with@(a@(internet@camera))@girl)','with a@(with@(a@(internet@camera))@girl)')).
+ind_rel(isa_wn(tiredly,slowly)).
+ind_rel(isa_wn(through,on)).
+ind_rel(isa_wn(through,in)).
+ind_rel(isa_wn(the,one)).
+ind_rel(isa_wn(tennis,'ping pong')).
+ind_rel(isa_wn(tattoo,'hand of@(a@teenager)')).
+ind_rel(isa_wn('tattered volleyball','broken volleyball')).
+ind_rel(isa_wn(tan,brown)).
+ind_rel(isa_wn(talk,chat)).
+ind_rel(isa_wn(take,relax)).
+ind_rel(isa_wn(take,pick)).
+ind_rel(isa_wn('t-shirt',dyed)).
+ind_rel(isa_wn(swimming,green)).
+ind_rel(isa_wn(sunglass,'dark glass')).
+ind_rel(isa_wn(sugar,spice)).
+ind_rel(isa_wn(stunt,trick)).
+ind_rel(isa_wn(stuffed,toy)).
+ind_rel(isa_wn(strum,play)).
+ind_rel(isa_wn(stream,'rocky stream')).
+ind_rel(isa_wn(stir,beat)).
+ind_rel(isa_wn(stick,swing)).
+ind_rel(isa_wn(statue,'gold statue')).
+ind_rel(isa_wn(star,sky)).
+ind_rel(isa_wn(stand,wade)).
+ind_rel(isa_wn(squirrel,'squirrel in@(s@circle)@(spin@around)')).
+ind_rel(isa_wn(sprinkle,put)).
+ind_rel(isa_wn(spray,water)).
+ind_rel(isa_wn(snowboarder,'snowboarder over@(a@(mound@(of@(a@snow))))@(do@(a@flip))')).
+ind_rel(isa_wn(snow,snowy)).
+ind_rel(isa_wn(snow,'snow of@(the@(beautiful@(snowy@landscape)))')).
+ind_rel(isa_wn(small,'eat a@carrot')).
+ind_rel(isa_wn(small,'devour a@carrot')).
+ind_rel(isa_wn(slowly,tiredly)).
+ind_rel(isa_wn(slide,crawl)).
+ind_rel(isa_wn(slice,bread)).
+ind_rel(isa_wn(sing,'sing the@guitar')).
+ind_rel(isa_wn(shrewd,'shrewdly be@(dig@(the@earth))')).
+ind_rel(isa_wn(shore,beach)).
+ind_rel(isa_wn(shop,building)).
+ind_rel(isa_wn(shirt,'dye black')).
+ind_rel(isa_wn(ship,boat)).
+ind_rel(isa_wn(shades,sunglass)).
+ind_rel(isa_wn(seashore,beach)).
+ind_rel(isa_wn(scratch,stroke)).
+ind_rel(isa_wn(say,talk)).
+ind_rel(isa_wn('s person','several person')).
+ind_rel(isa_wn('s cheerleader','many cheerleader')).
+ind_rel(isa_wn(s,two)).
+ind_rel(isa_wn(run_down,run)).
+ind_rel(isa_wn(run,sprint)).
+ind_rel(isa_wn(road,path)).
+ind_rel(isa_wn(ringer,wrestler)).
+ind_rel(isa_wn(ride,race)).
+ind_rel(isa_wn(race,racing)).
+ind_rel(isa_wn(rabbit,bunny)).
+ind_rel(isa_wn(quickly,'very fast')).
+ind_rel(isa_wn(put,sprinkle)).
+ind_rel(isa_wn(put,pack)).
+ind_rel(isa_wn(purple,colored)).
+ind_rel(isa_wn(puppy,'puppy rolling')).
+ind_rel(isa_wn(practice,play)).
+ind_rel(isa_wn(pot,'on the@stove')).
+ind_rel(isa_wn(pot,bowl)).
+ind_rel(isa_wn(post,pole)).
+ind_rel(isa_wn(porch,skateboard)).
+ind_rel(isa_wn(polish,clean)).
+ind_rel(isa_wn('point@(the@hand)','stand@up')).
+ind_rel(isa_wn('play the@drum','practice the@drum')).
+ind_rel(isa_wn('play in@(the@park)',boy)).
+ind_rel(isa_wn(play,swing)).
+ind_rel(isa_wn(play,strum)).
+ind_rel(isa_wn(play,slide)).
+ind_rel(isa_wn(plastic,toy)).
+ind_rel(isa_wn(place,'put into@(a@(pot@(of@(a@water))))')).
+ind_rel(isa_wn(place,boil)).
+ind_rel(isa_wn(piece,'mixer butter')).
+ind_rel(isa_wn(piece,'by a@person')).
+ind_rel(isa_wn(piano,keyboard)).
+ind_rel(isa_wn(person,'with@(a@spoon) person')).
+ind_rel(isa_wn(person,'one typing')).
+ind_rel(isa_wn(person,man)).
+ind_rel(isa_wn(person,'large person')).
+ind_rel(isa_wn(person,'football player')).
+ind_rel(isa_wn(person,cyclist)).
+ind_rel(isa_wn(person,'butter a@tray')).
+ind_rel(isa_wn(person,biker)).
+ind_rel(isa_wn(performer,'costumed performer')).
+ind_rel(isa_wn(parrot,'speaking parrot')).
+ind_rel(isa_wn(panda,'eating bamboo')).
+ind_rel(isa_wn(panda,cub)).
+ind_rel(isa_wn(pan,skillet)).
+ind_rel(isa_wn(pan,pot)).
+ind_rel(isa_wn(pace,'be walk')).
+ind_rel(isa_wn(outside,around)).
+ind_rel(isa_wn(open,bite)).
+ind_rel(isa_wn('on a@(masonry@pyramid)','off a@(masonry@pyramid)')).
+ind_rel(isa_wn(on,in)).
+ind_rel(isa_wn('olive oil','cooking oil')).
+ind_rel(isa_wn('off a@(dock@(into@(a@water)))','from a@(dock@(into@(a@water)))')).
+ind_rel(isa_wn('off a@(dock@(into@(a@pond)))','on a@(dock@(into@(a@pond)))')).
+ind_rel(isa_wn(note,paper)).
+ind_rel(isa_wn(motorcycle,motorbike)).
+ind_rel(isa_wn(monkey,chimp)).
+ind_rel(isa_wn(mix,stir)).
+ind_rel(isa_wn(meat,'into a@pan')).
+ind_rel(isa_wn(market,store)).
+ind_rel(isa_wn(man,racer)).
+ind_rel(isa_wn(man,player)).
+ind_rel(isa_wn(man,patient)).
+ind_rel(isa_wn(man,'on the@beach')).
+ind_rel(isa_wn(man,musician)).
+ind_rel(isa_wn(man,motorcyclist)).
+ind_rel(isa_wn(man,model)).
+ind_rel(isa_wn(man,'man on@(the@beach)')).
+ind_rel(isa_wn(man,hiker)).
+ind_rel(isa_wn(man,guy)).
+ind_rel(isa_wn(man,'eat a@cereal')).
+ind_rel(isa_wn(man,'drawing man')).
+ind_rel(isa_wn(man,biker)).
+ind_rel(isa_wn(make,speak)).
+ind_rel(isa_wn(magician,man)).
+ind_rel(isa_wn(lunge,jump)).
+ind_rel(isa_wn('look@(in@(the@(same@(inside@(a@(bamboo@structure))@direction))))','look@(in@(the@(same@(inside@(a@(bamboo@structure))@direction)))) attentively')).
+ind_rel(isa_wn('look on',look)).
+ind_rel(isa_wn(look,study)).
+ind_rel(isa_wn(look,'stare attentively')).
+ind_rel(isa_wn(look,stare)).
+ind_rel(isa_wn(lone,'be@(jump@(in@(the@air))) alone')).
+ind_rel(isa_wn(liner,cosmetic)).
+ind_rel(isa_wn(lick,'drink hungrily')).
+ind_rel(isa_wn(lemur,'eating lemur')).
+ind_rel(isa_wn(left,side)).
+ind_rel(isa_wn(lawn,grass)).
+ind_rel(isa_wn(large,'large small@(grey@dog)')).
+ind_rel(isa_wn(large,color)).
+ind_rel(isa_wn(lady,'topless girl')).
+ind_rel(isa_wn(kitten,cat)).
+ind_rel(isa_wn('kid in@(a@green)',boy)).
+ind_rel(isa_wn(kick,'kick box@(with@(a@trainer))')).
+ind_rel(isa_wn(keyboard,piano)).
+ind_rel(isa_wn(kangaroo,'baby kangaroo')).
+ind_rel(isa_wn('jump@(a@rope)','outside jump@(a@rope)')).
+ind_rel(isa_wn(jump,bounce)).
+ind_rel(isa_wn(jersey,'black jersey')).
+ind_rel(isa_wn('into@(a@container) chop',butter)).
+ind_rel(isa_wn('into@(a@bowl) crack',egg)).
+ind_rel(isa_wn(into,in)).
+ind_rel(isa_wn(in_front_of,near)).
+ind_rel(isa_wn(in,with)).
+ind_rel(isa_wn(in,through)).
+ind_rel(isa_wn(in,on)).
+ind_rel(isa_wn(in,into)).
+ind_rel(isa_wn(in,at)).
+ind_rel(isa_wn(hose,'next_to@(a@man) hose')).
+ind_rel(isa_wn('hold@(every@other)','together dance')).
+ind_rel(isa_wn(hit,knock)).
+ind_rel(isa_wn(hit,kick)).
+ind_rel(isa_wn(hedgehog,'small animal')).
+ind_rel(isa_wn(heal,revive)).
+ind_rel(isa_wn(have,eat)).
+ind_rel(isa_wn(have,color)).
+ind_rel(isa_wn(hat,'cowboy hat')).
+ind_rel(isa_wn('harmlessly fall',fall)).
+ind_rel(isa_wn(happy,'look happy')).
+ind_rel(isa_wn('hang out',run)).
+ind_rel(isa_wn(hair,'she hair')).
+ind_rel(isa_wn(guy,bloke)).
+ind_rel(isa_wn(guitar,singing)).
+ind_rel(isa_wn(group,pack)).
+ind_rel(isa_wn(group,crowd)).
+ind_rel(isa_wn(grass,lawn)).
+ind_rel(isa_wn(grass,field)).
+ind_rel(isa_wn(funny,'look funny')).
+ind_rel(isa_wn(fountain,'ground fountain')).
+ind_rel(isa_wn(food,meal)).
+ind_rel(isa_wn(fly,'fly_over a_few@rock')).
+ind_rel(isa_wn(flip,jump)).
+ind_rel(isa_wn(fix,apply)).
+ind_rel(isa_wn(fit,fix)).
+ind_rel(isa_wn(fit,apply)).
+ind_rel(isa_wn(female,girl)).
+ind_rel(isa_wn(escort,lead)).
+ind_rel(isa_wn(elder,elderly)).
+ind_rel(isa_wn(eat,bite)).
+ind_rel(isa_wn(drop,'into@(a@pot) drop')).
+ind_rel(isa_wn('drill a@hole','make a@hole')).
+ind_rel(isa_wn(drawing,tattoo)).
+ind_rel(isa_wn(dog,'bull dog')).
+ind_rel(isa_wn(dive,jump)).
+ind_rel(isa_wn(dice,slice)).
+ind_rel(isa_wn(device,telephone)).
+ind_rel(isa_wn(cut,slice)).
+ind_rel(isa_wn(cut,dice)).
+ind_rel(isa_wn(cut,'chop up')).
+ind_rel(isa_wn(cut,'chop down')).
+ind_rel(isa_wn(cut,chop)).
+ind_rel(isa_wn(crowd,person)).
+ind_rel(isa_wn(cord,rope)).
+ind_rel(isa_wn('cook slice','chop up')).
+ind_rel(isa_wn(cook,roast)).
+ind_rel(isa_wn(container,'plastic container')).
+ind_rel(isa_wn(container,box)).
+ind_rel(isa_wn(construction,work)).
+ind_rel(isa_wn(compete,race)).
+ind_rel(isa_wn(cluster,sit)).
+ind_rel(isa_wn(clown,'clown singing')).
+ind_rel(isa_wn('climb down',climb)).
+ind_rel(isa_wn(clean,erase)).
+ind_rel(isa_wn(chubby,boy)).
+ind_rel(isa_wn(chop,'garlic dice')).
+ind_rel(isa_wn(child,'young child')).
+ind_rel(isa_wn(child,'blond child')).
+ind_rel(isa_wn(chase,catch)).
+ind_rel(isa_wn(cat,kitten)).
+ind_rel(isa_wn(cartoon,animated)).
+ind_rel(isa_wn(carry,bite)).
+ind_rel(isa_wn(car,racing)).
+ind_rel(isa_wn(car,'burning car')).
+ind_rel(isa_wn(cap,hat)).
+ind_rel(isa_wn('camera against@(a@nail)',lens)).
+ind_rel(isa_wn(camera,webcam)).
+ind_rel(isa_wn(camera,photo)).
+ind_rel(isa_wn('by themselves',together)).
+ind_rel(isa_wn('by another@woman','to another@woman')).
+ind_rel(isa_wn(build,stone)).
+ind_rel(isa_wn(brush,style)).
+ind_rel(isa_wn(brown,colored)).
+ind_rel(isa_wn(break,'into@(a@bowl) crack')).
+ind_rel(isa_wn(boy,'young boy')).
+ind_rel(isa_wn(boy,swimmer)).
+ind_rel(isa_wn(boy,guy)).
+ind_rel(isa_wn(blue,colored)).
+ind_rel(isa_wn(bikini,'swimming suite')).
+ind_rel(isa_wn(bear,'eating bamboo')).
+ind_rel(isa_wn(beach,shore)).
+ind_rel(isa_wn(beach,sand)).
+ind_rel(isa_wn('be@walk','outside be@walk')).
+ind_rel(isa_wn('be@(hold@up@(a@sign))','pitifully be@(beg@(for@(a@money)))')).
+ind_rel(isa_wn('be@(climb@(a@(steep@hill)))','wildly be@(climb@(a@(steep@hill)))')).
+ind_rel(isa_wn('be@(catch@(a@(tiny@(bit@(of@(a@air))))))',dress)).
+ind_rel(isa_wn('at s@recruit','to s@recruit')).
+ind_rel(isa_wn(at,in)).
+ind_rel(isa_wn(area,'snowy area')).
+ind_rel(isa_wn(aim,handle)).
+ind_rel(isa_wn(aim,draw)).
+ind_rel(isa_wn(acrobatics,trick)).
+ind_rel(isa_wn(accessory,military)).
+ind_rel(isa_wn('a_few ferret','several ferret')).
+ind_rel(isa_wn(a,two)).
+ind_rel(isa_wn(a,three)).
+ind_rel(isa_wn(a,four)).
+ind_rel(disj(sit,wade)).
+ind_rel(disj(set,walk)).
+ind_rel(disj(run,stop)).
+ind_rel(disj('ride a@motorcycle',stand)).
+ind_rel(disj('put on',remove)).
+ind_rel(disj(put,reveal)).
+ind_rel(disj(pour,'take out')).
+ind_rel(disj(pour,remove)).
+ind_rel(disj(player,'soccer game')).
+ind_rel(disj('play by@(a@girl)','put away')).
+ind_rel(disj(miss,strike)).
+ind_rel(disj('lie down','run around')).
+ind_rel(disj(lie,run)).
+ind_rel(disj(lie,play)).
+ind_rel(disj(jump,rest)).
+ind_rel(disj('ignore the@(front@(of@(the@class)))',listen)).
+ind_rel(disj('hit the@ball','miss the@ball')).
+ind_rel(disj(grass,hay)).
+ind_rel(disj(get,ride)).
+ind_rel(disj(follow,shun)).
+ind_rel(disj(fold,unfold)).
+ind_rel(disj(fishing,rod)).
+ind_rel(disj(fall,'ride the@wave')).
+ind_rel(disj('eye on@(the@eyelid)',pencil)).
+ind_rel(disj(empty,full)).
+ind_rel(disj('dirt bike','yellow bike')).
+ind_rel(disj('different team',player)).
+ind_rel(disj(deny,grant)).
+ind_rel(disj(close,drill)).
+ind_rel(disj('clean bike','yellow bike')).
+ind_rel(disj(clean,dirty)).
+ind_rel(disj('catch the@(ball@(in@(a@(mid@air))))','miss the@(ball@(in@(a@(mid@air))))')).
+ind_rel(disj(add,remove)).
+ind_rel(ant_wn(blonde,little)).
