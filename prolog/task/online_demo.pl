bbfa3fbacbbc383ca9d08ba16a05dc95748d592b
kovvalsky <lasha.abzianidze@gmail.com>
2025-07-03 03:13:09 +0200

    first attempt of cleaning the code

--- prolog/task/online_demo.pl
+++ prolog/task/online_demo.pl
@@ -29,7 +29,6 @@ online_demo(ID, Format) :-
 	%report(['Entering online_demo/1\n']),
 	%entail(1, _Answer, Provers_Answer, Closed, FinalStatus),
 	%print_problem(ID),
-	current_output(S),
 	problem_to_ttTerms('align', ID, Prems, Hypos, Align_Prems, Align_Hypos, KB),
 	set_rule_eff_order,
 	check_problem(KB-_, Align_Prems, Align_Hypos, 'yes', _, Al_Cl_yes, Al_St_yes, _, Al_Tr_yes),
@@ -37,53 +36,39 @@ online_demo(ID, Format) :-
 		check_problem(KB-_, Align_Prems, Align_Hypos, 'no', _,  Al_Cl_no, Al_St_no, _, Al_Tr_no),
 		check_problem(KB-_, Prems, Hypos, 'yes', _, Cl_yes, St_yes, _, Tr_yes),
 		check_problem(KB-_, Prems, Hypos, 'no', _,  Cl_no, St_no, _, Tr_no),
-		( Al_Cl_yes == 'closed' ->
-			Al_St_yes = (Al_St_yes_Ter, Al_St_yes_Num), 
-			YES = ['al', Al_Cl_yes, Al_St_yes_Ter, Al_St_yes_Num],
-			Tree_yes = Al_Tr_yes
-		; ( St_yes = (Ter1, N1) -> atomic_list_concat([Ter1,N1], St_yes0); St_yes0 = St_yes ),
-		  YES = ['na', Cl_yes, St_yes0],
-		  Tree_yes = Tr_yes
-		),
-		( Al_Cl_no ==  'closed' ->
-			Al_St_no = (Al_St_no_Ter, Al_St_no_Num),
-			NO  = ['al', Al_Cl_no,  Al_St_no_Ter, Al_St_no_Num],
-			Tree_no = Al_Tr_no
-		; ( St_no = (Ter2, N2) -> atomic_list_concat([Ter2,N2], St_no0); St_no0 = St_no ),
-		  NO  = ['na', Cl_no,  St_no0],
-		  Tree_no = Tr_no
-		),
-		atomic_list_concat(['yes' | YES], '_', Yes_File),
-		atomic_list_concat([ 'no' | NO],  '_', No_File),
+		summarize_align_closed_status(Al_Cl_yes, Al_St_yes, Al_Tr_yes, Cl_yes, St_yes, Tr_yes, YES, Tree_yes),
+		summarize_align_closed_status(Al_Cl_no, Al_St_no, Al_Tr_no, Cl_no, St_no, Tr_no, NO, Tree_no),
 		( memberchk([Al,'closed'|_], [YES,NO]) ->
 			memberchk(Al-Align, ['al'-'align', 'na'-'no_align']),
-			write_parsed_problem(S, Format, Align, ID)
-		; write_parsed_problem(S, Format, 'no_align', ID)
-		),
-		write_proof_tree(S, Format, Tree_yes, ID),
-		write_proof_tree(S, Format, Tree_no,  ID),
-		( Format == 'xml' -> format('~w, ~w~n', [Yes_File, No_File]); true )
-	; YES = 'yes_NA', NO = 'no_NA',
-		write_parsed_problem(S, Format, 'no_align', ID),
-		( Format == 'xml' ->
-			write(S, '\n<tableau>no tableau</tableau>\n<tableau>no tableau</tableau>\n'),
+		; 	Align = 'no_align'
+		)
+	; YES = 'yes_NA', NO = 'no_NA', Align = 'no_align'
+	).
+
+% sumarizes the results with aligned and non-aligned terms
+summarize_align_closed_status(Al_Cl, Al_St, Al_Tr, Cl, St, Tr, Ans, Tree) :-
+	( Al_Cl == 'closed' ->
+		Al_St = (Al_St_Ter, Al_St_Num), 
+		Ans = ['al', Al_Cl, Al_St_Ter, Al_St_Num],
+		Tree = Al_Tr
+	; ( St = (Ter1, N1) -> atomic_list_concat([Ter1,N1], St0); St0 = St ),
+		Ans = ['na', Cl, St0],
+		Tree = Tr
+	).
+
+write_problem_proof('xml', YES, NO, Align, Tree_yes, Tree_no, ID) :-
+	current_output(S),
+	write_parsed_problem_as_xml(S, Align, ID),
+	( YES \== 'yes_NA' ->
+		write_xml_proof_tree(S, Tree_yes, ID),
+		write_xml_proof_tree(S, Tree_no, ID),
+		atomic_list_concat(['yes' | YES], '_', Yes_File),
+		atomic_list_concat([ 'no' | NO],  '_', No_File),
+		format('~w, ~w~n', [Yes_File, No_File]),		
+	; write(S, '\n<tableau>no tableau</tableau>\n<tableau>no tableau</tableau>\n'),
 		 	format('~w, ~w~n', [YES, NO])
-		; true )
 	),
 	close(S).
-
-% writing proof trees in xml or json format
-write_proof_tree(S, 'xml', Tree, PID) :- !,
-	write_xml_proof_tree(S, Tree, PID).
-
-write_proof_tree(S, 'json', Tree, PID) :- !,
-	write_json_proof_tree(S, Tree, PID).
-
-write_parsed_problem(S, 'xml', Align, ID) :- !,
-	write_parsed_problem_as_xml(S, Align, ID).
-
-write_parsed_problem(S, 'json', Align, ID) :- !,
-	write_parsed_problem_as_json(S, Align, ID).	
 
 
 print_problem(ID) :-
